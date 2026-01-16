52a1ccdf17eea58a0aa532cc1f90260ae4118505
45-Akash <akashkanni143@gmail.com>
2026-01-16 21:08:02 +0530

    Intro to DS

---
+++ Intoruduction_to_DS/cocept.cpp
@@ -0,0 +1,46 @@
+/*
+
+------------------------Data Structure-----------------------
+
+1. What is that
+=>
+Data Structurte :- It is a way of organising and storing the data
+                   in such a way, that it helps in efficient
+                   storage, retitrieval, deletion and updation of data.
+
+                   It helps in problem solving
+
+2. Why do we need to learn that
+=> To be able to solve coding questions in interviews and online assesgemnt
+   once you are able to solve questions......your chances to secure a good
+   paying job (10+ LPA) will rise by almost 90 percent.
+
+3. How to learn
+=> From CodeVerse academy by Akash Kanni....
+
+
+
+-------------------------Catogories of Data Structure---------------------
+
+primitive DS :- These are pre-existing (already defined/avaialable data structures)
+                easy to use
+                less-complex
+                fixed size
+Example : Int, float, boolean, char, double
+
+non-primitve (derived) DS :- These are derived data structures which uses primitve ds
+                             as part of it.
+                             Array
+                             stack
+                             queue
+                             linked-list
+                             graph
+                             tree
+
+
+                             Hash table
+                             Hashmap
+                             trie
+            
+
+*/
