9b25323c06981dc11895cd9ecefb8877fc1d5a0b
45-Akash <akashkanni143@gmail.com>
2026-03-17 21:03:18 +0530

    Yukti-2.0

---
+++ Yukti-2.0/Introduction_DS/concept.cpp
@@ -0,0 +1,53 @@
+/*
+
+Did you use google regulry?  --> Yes
+
+How does Google manages to serve the request so fast
+You serached "The best netflix series of 2025"
+
+--> It is all possible due to the right implementation of data structures and
+    the efficient Algorithms
+    To build an efficient algorithm the developer should be proeficient with 
+    DATA STRUCTURE
+
+What are your goals and expectation from this course?
+--> Those who attend this course should/will be more than enough prepared for any job 
+    interviews and coding exams.
+
+Importance of DS for getting JOB :- 
+
+- To get a job (18k-20k) VS To get a dream package (10LPA)
+- 80% and plus job interviews are based on DSA
+
+- Because of my personal experience and Industrial Inside knowledge
+- My first interview question was regarding DP (Dynamic Programming) (60 mins)
+- And i was able to solve it efficiently in 20 mins
+- Interview got over
+
+
+What is Data structure :- 
+- Organize and manage data efficiently
+- data structure is a way of organizing data to perform operations efficiently
+- Data structure is a systematic way to organise, store and manage data in computer efficiently
+
+- Data Structure is a way to store and organize the data efficiently. 
+Consider you have 10 iters of milk --> assume milk is your data
+You want to store milk --> which storage option you will choose
+
+-- 1. To choose milk polythene bag (The bags size is of 12 liter capacity)
+-- 2. To choose a tanker
+-- 3. To store it in cloth bag
+
+Tell me your preferences --> 1 / 2 / 3
+
+
+// Do you have VS code installed on your machine?
+// And does it compatible to run CPP code?
+
+
+
+// How to write efficient code
+// Time complexity and space complexity O(n)
+// Types of Data structure
+
+*/
